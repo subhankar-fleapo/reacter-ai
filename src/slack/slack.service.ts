2f328ae4b58756e0931a97efb676d9a0c6fd742b
richard-fleapo <richard@fleapo.com>
2026-01-09 20:15:27 +0530

    feat: send messages using slack

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { App } from '@slack/bolt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SlackService {
-  constructor() {}
+  private app: App;
+
+  constructor(private readonly configService: ConfigService) {
+    this.app = new App({
+      token: this.configService.get('SLACK_BOT_TOKEN'),
+      signingSecret: this.configService.get('SLACK_SIGNING_SECRET'),
+    });
+  }
 
   async generateResponse(): Promise<string> {
     return 'hello';
+  }
+
+  async sendMessage(channel: string, text: string) {
+    await this.app.client.chat.postMessage({
+      channel,
+      text,
+    });
   }
 }
